a59f0fd6fc440dde03db6e78ac3a57c50fe4fc1f
Uthman Al-Ayek Eqbal <135247149+uthm4n@users.noreply.github.com>
2023-10-27 17:58:39 +0100

    Update and rename get-subtenant-roles.py to get-available-account-roles-for-subtenant.py

--- get-available-account-roles-for-subtenant.py
+++ get-available-account-roles-for-subtenant.py
@@ -17,7 +17,7 @@ headers = {
 response = requests.get(url, headers=headers, verify=False)
 data = response.json()
 
-subtenantRolesQuery = parse("$.roles[?(@.ownerId[*] != 1)]")       # only retrieve subtenant roles
+subtenantRolesQuery = parse("$.roles[?(@.scope[*] = 'Account' & @.roleType = 'account')]")       # only retrieve subtenant roles
 subtenantRoles = [match.value for match in subtenantRolesQuery.find(data)]
 
 print(json.dumps(subtenantRoles, indent=2))
